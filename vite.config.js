6ce51f16eb9969224d37f29f9571f51417766e5c
ritesh-intellisys <ritesh.intellisys@gmail.com>
2025-05-23 21:03:41 +0530

    lsdkjflslfds

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chartist-demo/',
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  
 })
